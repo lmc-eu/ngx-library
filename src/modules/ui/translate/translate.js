de5d9cf8db78b0851dee0c59da3bb546d1670d8d
Pavol Daniš <palto@palto.sk>
2012-08-28 20:28:39 +0200

    ngx.ui.translate supports bindings

--- src/modules/ui/translate/translate.js
+++ src/modules/ui/translate/translate.js
@@ -3,15 +3,29 @@
 
     var module = angular.module('ngx.ui.translate', ['ngx', 'ngx.dictionary']);
 
-    module.directive('ngxTranslate', ['ngxDictionary', '$compile', function(ngxDictionary, $compile) {
+    var markupSymbol;
+
+    module.config(['$interpolateProvider', function($interpolateProvider) {
+        markupSymbol = $interpolateProvider.startSymbol();
+    }]);
+
+    module.directive('ngxTranslate', ['ngxDictionary', '$interpolate', '$log', function(ngxDictionary, $interpolate) {
         return {
             link: function(scope, element, attrs) {
                 var key = (attrs.ngxTranslate ? attrs.ngxTranslate : element.html());
                 if (key.length) {
-                    element.html(ngxDictionary(key, attrs.language));
-//                    if (key.indexOf('{{') !== -1) {
-//                        $compile(element)(scope);
-//                    }
+                    var translated = ngxDictionary(key, attrs.language);
+
+                    // handle bindings in translation
+                    if (translated.indexOf(markupSymbol) !== -1) {
+                        scope.$watch(function() {
+                            return $interpolate(translated)(scope);
+                        }, function(value) {
+                            element.html(value);
+                        });
+                    }
+
+                    element.html(translated);
                 }
             }
         };
