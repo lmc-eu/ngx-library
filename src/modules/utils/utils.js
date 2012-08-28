a3d5b169c33a7ec26c399a32810a432b7aa0a343
Pavol Daniš <palto@palto.sk>
2012-08-28 18:12:05 +0200

    ngx.utils - useful util functions

---
+++ src/modules/utils/utils.js
@@ -0,0 +1,32 @@
+(function(angular) {
+    'use strict';
+
+    var ngxUtils = {};
+
+    /**
+     * Recursive angular.extend
+     * @param dst
+     * @param src
+     * @return {Object}
+     */
+    ngxUtils.extendRecursive = function(dst, src) {
+        angular.forEach(arguments, function(obj, index) {
+            if (index === 0) {
+                return;
+            }
+            angular.forEach(obj, function(value, property) {
+                if (angular.isObject(value) && angular.isObject(dst[property])) {
+                    ngxUtils.extendRecursive(dst[property], value);
+                } else {
+                    dst[property] = value;
+                }
+            });
+        });
+
+        return dst;
+    };
+
+    angular.module('ngx.utils', ['ngx'])
+       .value('ngxUtils', ngxUtils);
+
+})(window.angular);
