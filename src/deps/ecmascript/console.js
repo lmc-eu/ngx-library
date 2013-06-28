c3dcc27c7a958b9f6c3508e4e82a2a485a4776cf
Josef Zamrzla <josef.zamrzla@gmail.com>
2013-06-28 16:54:56 +0200

    window.console failback for legacy browsers

---
+++ src/deps/ecmascript/console.js
@@ -0,0 +1,2 @@
+/* Missing window.console failback */
+if(!window.console){var names=["log","debug","info","warn","error","assert","dir","dirxml","group","groupEnd","time","timeEnd","count","trace","profile","profileEnd"];window.console={};for(var i=0;i<names.length;++i){window.console[names[i]]=function(){}}}
