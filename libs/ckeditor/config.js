e0077f29a7c9a8978d1bfb5d627c3421382c7e10
dedicj <jiri.dedic@lmc.eu>
2013-11-18 09:46:28 +0100

    new ckeditor

--- libs/ckeditor/config.js
+++ libs/ckeditor/config.js
@@ -1,10 +1,10 @@
-﻿/*
-Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
-For licensing, see LICENSE.html or http://ckeditor.com/license
-*/
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
 
-CKEDITOR.editorConfig = function( config )
-{
-    config.language = 'cs';
-    config.skin = 'lmc';
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
 };
